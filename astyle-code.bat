93d05f0f05094865caae04312a49ea84a774b40e
Robert Beckebans <robert.beckebans@gmail.com>
2020-09-09 16:05:28 +0200

    Added Astyle 3.1

---
+++ astyle-code.bat
@@ -0,0 +1,11 @@
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.h
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.c
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.cpp
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.glsl
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.vert
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.frag
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.tesc
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.tese
+astyle.exe -v --formatted --options=astyle-options.ini --recursive *.comp
+
+pause
